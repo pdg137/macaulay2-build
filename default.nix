f873d9d50847c259c16b3a3a4d853a0658d3c2b1
Paul Grayson <pdg@alum.mit.edu>
2024-02-03 06:57:28 -0800

    more dependencies

--- default.nix
+++ default.nix
@@ -1,54 +1,28 @@
 let
   pkgs = import <nixpkgs> {};
 
-  tarfile_libatomic = pkgs.fetchurl {
-    url = "https://www.hboehm.info/gc/gc_source/libatomic_ops-7.6.2.tar.gz";
-    hash = "sha256-IZck7a09WA1NN7IuHXy1LwAG0oLSapuGgbVgpiUULuY=";
+  m2download = args: pkgs.fetchurl {
+    url = "http://macaulay2.com/Downloads/OtherSourceCode/${builtins.elemAt args 0}";
+    hash = builtins.elemAt args 1;
   };
 
-  tarfile_gc = pkgs.fetchurl {
-    url = "https://www.hboehm.info/gc/gc_source/gc-8.0.4.tar.gz";
-    hash = "sha256-Q2oN3GexrAsEBbYalnW8qeB1yBVvTevR0G86VsfNKJ0=";
-  };
-
-  tarfile_gdbm = pkgs.fetchurl {
-    url = "ftp://prep.ai.mit.edu/gnu/gdbm/gdbm-1.9.1.tar.gz";
-    hash = "sha256-YCWFJjd3KwaZ8ilLXxT9SghLyjyBYdKdZNHzDW0amu0=";
-  };
-
-  tarfile_gmp = pkgs.fetchurl {
-    url = "ftp://ftp.gnu.org/gnu/gmp/gmp-6.1.0.tar.bz2";
-    hash = "sha256-SYRJqZTv66UniFwQQFmTQnmV0/hrh2jYzfjZ3Xxrc+g=";
-  };
-
-  tarfile_mpfr = pkgs.fetchurl {
-    # updated versions available at http://www.mpfr.org/
-    url = "http://macaulay2.com/Downloads/OtherSourceCode/mpfr-4.0.2.tar.xz";
-    hash = "sha256-HTvnCGBOrg5C1Xi6k7OQwqFF8XdDp0TY8/jCrVhVo4o=";
-  };
-
-  tarfile_mpfi = pkgs.fetchurl {
-    # original seems a little different:
-    # https://gitlab.inria.fr/mpfi/mpfi/-/archive/1.5.4/mpfi-1.5.4.tar.gz
-    url = "http://macaulay2.com/Downloads/OtherSourceCode/mpfi-1.5.4.tar.gz";
-    hash = "sha256-MuatUpyXqlzgPijQHJIdG84aRk+0xX+8JI174h5lJ4I=";
-  };
-
-  tarfile_readline = pkgs.fetchurl {
-    url = "ftp://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz";
-    hash = "sha256-P+txcfFqhO6CyhijbXub4QmlLAT0kqBTMx19EJUAfDU=";
-  };
-
-  tarfile_ntl = pkgs.fetchurl {
-    # note: later releases available on https://github.com/libntl/ntl
-    url = "https://libntl.org/ntl-10.5.0.tar.gz";
-    hash = "sha256-uQs2yd2JVMm8VEELHVfAC+lWrh21oGKUWCK716hqtNI=";
-  };
-
-  tarfile_flint = pkgs.fetchurl {
-    url = "https://flintlib.org/flint-2.8.4.tar.gz";
-    hash = "sha256-Yd+S6oyOncaS1Gxx1/UKqgmjPUugjQKheEcwpEXl5L4=";
-  };
+  downloads = map m2download [
+    ["cddlib-0.94m.tar.gz" "sha256-cN/9szabhwTcdUKKGzxCq5BHuBzgOfEvQn4usrGw3uI="]
+    ["factory-4.2.1.tar.gz" "sha256-OjE12Nnom8pRKyLIhY8+A/RLFWKd9vAwnOT33e3QmhU="]
+    ["flint-2.8.4.tar.gz" "sha256-Yd+S6oyOncaS1Gxx1/UKqgmjPUugjQKheEcwpEXl5L4="]
+    ["frobby_v0.9.0.tar.gz" "sha256-rwkjg+bchJyG9OeXR64OXNMJppB0cjDhCqONYGQAYt8="]
+    ["gc-8.0.4.tar.gz" "sha256-Q2oN3GexrAsEBbYalnW8qeB1yBVvTevR0G86VsfNKJ0="]
+    ["gdbm-1.9.1.tar.gz" "sha256-YCWFJjd3KwaZ8ilLXxT9SghLyjyBYdKdZNHzDW0amu0="]
+    ["glpk-4.59.tar.gz" "sha256-45i+Lny4qYWEMlJocEcphyVYpKiFVbyKVBOdAX65664="]
+    ["gmp-6.1.0.tar.bz2" "sha256-SYRJqZTv66UniFwQQFmTQnmV0/hrh2jYzfjZ3Xxrc+g="]
+    ["libatomic_ops-7.6.2.tar.gz" "sha256-IZck7a09WA1NN7IuHXy1LwAG0oLSapuGgbVgpiUULuY="]
+    ["mpfi-1.5.4.tar.gz" "sha256-MuatUpyXqlzgPijQHJIdG84aRk+0xX+8JI174h5lJ4I="]
+    ["mpfr-4.0.2.tar.xz" "sha256-HTvnCGBOrg5C1Xi6k7OQwqFF8XdDp0TY8/jCrVhVo4o="]
+    ["mpsolve-3.2.1.tar.gz" "sha256-PRFCiumrLgIPJMq/vNnk2bIuxXLPcK8NRP6Nrh1R544="]
+    ["lapack-3.9.0.tgz" "sha256-EGCH8btfRq/fun9WnQy+I9rLmgfNJHM3ZaDonb4a1XM="]
+    ["ntl-10.5.0.tar.gz" "sha256-uQs2yd2JVMm8VEELHVfAC+lWrh21oGKUWCK716hqtNI="]
+    ["readline-8.2.tar.gz" "sha256-P+txcfFqhO6CyhijbXub4QmlLAT0kqBTMx19EJUAfDU="]
+  ];
 
   link_tarfile =
     file: "ln -s ${file.outPath} src/M2/BUILD/tarfiles/${file.name};";
@@ -78,19 +52,7 @@ in
     ];
 
     link_downloads =
-      map
-        link_tarfile
-        [
-          tarfile_libatomic
-          tarfile_gc
-          tarfile_gdbm
-          tarfile_gmp
-          tarfile_mpfr
-          tarfile_mpfi
-          tarfile_ntl
-          tarfile_readline
-          tarfile_flint
-        ];
+      map link_tarfile downloads;
 
     configureArgs = [
       "--with-boost=${pkgs.boost.dev}"
